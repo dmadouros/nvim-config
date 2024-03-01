9bb826b14ebc5bcdcfefc340a60552cd5a9cd3ef
David Madouros <david.madouros@gmail.com>
2024-02-29 23:43:19 -0700

    Update obsidian keymaps

--- lua/custom/plugins/obsidian_keymaps.lua
+++ lua/custom/plugins/obsidian_keymaps.lua
@@ -1,10 +1,12 @@
 return {
   vim.keymap.set('n', '<leader>on', '<cmd>ObsidianNew<CR>', {});
+  vim.keymap.set('v', '<leader>ox', '<cmd>ObsidianLink<CR>', {});
   vim.keymap.set('v', '<leader>ol', '<cmd>ObsidianLinkNew<CR>', {});
   vim.keymap.set('n', '<leader>oo', '<cmd>ObsidianOpen<CR>', {});
   vim.keymap.set('n', '<leader>oq', '<cmd>ObsidianQuickSwitch<CR>', {});
   vim.keymap.set('n', '<leader>od', '<cmd>ObsidianToday<CR>', {});
   vim.keymap.set('n', '<leader>ot', '<cmd>ObsidianTags<CR>', {});
+  vim.keymap.set('n', '<leader>ow', '<cmd>ObsidianWorkspace<CR>', {});
   vim.keymap.set('i', 'jj', '<ESC>', {});
   vim.keymap.set("n", "<leader>nd", "<cmd>NoiceDismiss<CR>", {desc = "Dismiss Noice Message"});
 }
